dc65cbd1704855753317e9036a02f268be7f11b2
MatejB <matej@nivas.hr>
2015-01-24 11:15:24 +0100

    creatino of storage location on init

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
-import "runtime"
+import (
+	"log"
+	"os"
+	"runtime"
+)
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	err := createStorage()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 
+}
+
+func createStorage() error {
+	paths := []string{
+		"./storage/datastore",
+		"./storage/.upload"}
+
+	for _, path := range paths {
+		err := os.MkdirAll(path, 02750)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
