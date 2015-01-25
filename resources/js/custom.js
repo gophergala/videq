7f37ac6daa61cc2c0903a7399865a1dbafcc8e2b
luzel <luka.uzel@gmail.com>
2015-01-25 13:35:50 +0100

    js screens, screen home, screen checkout,js messages, success, error, info

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -13,10 +13,10 @@ var Video = {
 		if( vid_cont.length > 0 && !Modernizr.touch && Modernizr.mq('only screen and (min-width: 781px)') ) {
 
 
-			var html = '<video autoplay loop poster="/resources/vid/loop.jpg">'+
-					   '<source src="/resources/vid/loop.mp4" type="video/mp4" />'+
-					   '<source src="/resources/vid/loop.webm" type="video/webm" />'+
-					   '<source src="/resources/vid/loop.ogv" type="video/ogg" />'+
+			var html = '<video autoplay loop poster="/resources/vid/loop.jpg">' +
+					   '<source src="/resources/vid/Videq-SW.mp4" type="video/mp4" />' +
+					   '<source src="/resources/vid/Videq-SW.webm" type="video/webm" />' +
+					   '<source src="/resources/vid/Videq-SW.ogv" type="video/ogg" />' +
 					   '</video>';
 
 			vid_cont.prepend(html);
@@ -26,11 +26,69 @@ var Video = {
 
 };
 
+var Screen = {
+
+	init : function () {
+
+		Screen.show("screen-drop-zone");
+	},
+
+	show : function (name) {
+
+		$(".screen").hide();
+		$(".screen." + name ).show();
+	
+	}
+
+};
+
+var Msg = {
+
+	init : function () {
+		this.hide();
+	},
+
+	hide : function () {
+		$(".flashmsg div").fadeOut();
+	},
+
+	show : function () {
+		$(".flashmsg div").fadeIn();
+	},
+
+	success : function (msg) {
+		this.hide();
+		$(".flashmsg .imoon").addClass("icon-circle-check");
+		$(".flashmsg div").attr("class", "").addClass("success");
+		$(".flashmsg div p").html(msg);
+		this.show();
+	},
+
+	error : function (msg) {
+		this.hide();
+		$(".flashmsg .imoon").addClass("icon-notification");
+		$(".flashmsg div").attr("class", "").addClass("error");
+		$(".flashmsg div p").html(msg);
+		this.show();
+	},
+
+	info : function (msg) {
+		this.hide();
+		$(".flashmsg .imoon").addClass("icon-info");
+		$(".flashmsg div").attr("class", "").addClass("info");
+		$(".flashmsg div p").html(msg);
+		this.show();
+	}
+
+};
+
 
 $(function(){
 
 
 	Video.init();
+	Screen.init();
+	Msg.init();
 
 	var notCompletedFiles = getFilesListFromCookie();
 	if (notCompletedFiles != false) {
